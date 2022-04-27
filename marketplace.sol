ea91e988e80425ee163e5150c60272a5f3061fe8
Tuan Anh Le <anh.le@myob.com>
2022-04-28 08:25:57 +1000

    feat: include ERC-20 interface

--- marketplace.sol
+++ marketplace.sol
@@ -2,6 +2,19 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+// Interface of an ERC-20 token so this contract can interact with it
+interface IERC20Token {
+  function transfer(address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address) external view returns (uint256);
+  function allowance(address, address) external view returns (uint256);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 contract Marketplace {  
 
     struct Product {
