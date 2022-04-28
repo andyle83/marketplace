19a37c4d01207b2cc9f3eba0794bc8f1874cf7b9
Tuan Anh Le <anh.le@myob.com>
2022-04-28 21:37:58 +1000

    feat: create boilerplace for front-end

---
+++ app/README.md
@@ -0,0 +1,36 @@
+# Install
+
+```
+
+npm install
+
+```
+
+or 
+
+```
+
+yarn install
+
+```
+
+# Start
+
+```
+
+npm run dev
+
+```
+
+# Build
+
+```
+
+npm run build
+
+```
+# Usage
+1. Install the [CeloExtensionWallet](https://chrome.google.com/webstore/detail/celoextensionwallet/kkilomkmpmkbdnfelcpgckmpcaemjcdh?hl=en) from the google chrome store.
+2. Create a wallet.
+3. Go to [https://celo.org/developers/faucet](https://celo.org/developers/faucet) and get tokens for the alfajores testnet.
+4. Switch to the alfajores testnet in the CeloExtensionWallet.
---
+++ contract/marketplace.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT  
+
+pragma solidity >=0.7.0 <0.9.0;
+
+// Interface of an ERC-20 token so this contract can interact with it
+interface IERC20Token {
+  function transfer(address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address) external view returns (uint256);
+  function allowance(address, address) external view returns (uint256);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract Marketplace {  
+
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    struct Product {
+        address payable owner;
+        string name;
+        string image;
+        string description;
+        string location;
+        uint price;
+        uint sold;
+    }
+
+    // keep track how many products
+    uint internal productsLength = 0;
+
+    mapping(uint => Product) internal products;
+
+    function writeProduct( 
+        string memory _name,
+        string memory _image,
+        string memory _description,
+        string memory _location,
+        uint _price
+    ) public {
+        // 0: is available, 1: is sold - not available
+        uint _sold = 0;
+
+        products[productsLength] = Product(
+            payable(msg.sender),
+            _name,
+            _image,
+            _description,
+            _location,
+            _price,
+            _sold
+        );
+
+        // increase whenever we have a new product added
+        productsLength++;
+    }
+
+    function getProductsLength() public view returns (uint) {
+        return productsLength;
+    }
+
+    function readProduct(uint _index) public view returns (
+        address payable,
+        string memory,
+        string memory,
+        string memory,
+        string memory,
+        uint,
+        uint
+    ) {
+        return (
+            products[_index].owner,
+            products[_index].name,
+            products[_index].image,
+            products[_index].description,
+            products[_index].location,
+            products[_index].price,
+            products[_index].sold
+        );
+    }
+
+    // buy a product from our contract
+    function buyProduct(uint _index) public payable {
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                products[_index].owner,
+                products[_index].price
+            ),
+            "Transfer failed"
+        );
+
+        products[_index].sold++;
+    }
+}
