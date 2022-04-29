1cd5eb725afde071d7aef37ae6c6de78048f2518
Tuan Anh Le <anh.le@myob.com>
2022-04-29 22:45:32 +1000

    fix: update webpack configure

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     main: path.resolve(process.cwd(), "src", "main.js")
   },
   output: {
-    path: path.resolve(process.cwd(), "docs"),
+    path: path.resolve(__dirname, "../docs/"),
     publicPath: ""
   },
 	node: {
