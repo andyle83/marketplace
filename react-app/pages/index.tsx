c3f7ea025625f99980fa14bda8885beeb3b11053
Tuan Anh Le <anh.le@myob.com>
2022-05-04 20:47:56 +1000

    feat: code clean up

--- react-app/pages/index.tsx
+++ react-app/pages/index.tsx
@@ -2,19 +2,8 @@ import * as React from "react";
 import { useContractKit } from "@celo-tools/use-contractkit";
 import AppLayout from "@/components/layout/AppLayout";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
 export default function App() {
   const { network } = useContractKit();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
 
   return (
     <AppLayout title="Marketplace" description="A commune marketplace">
