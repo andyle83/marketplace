44a011ae9b921b67bf8d9f3af45f2e7738a5393a
Tuan Anh Le <anh.le@myob.com>
2022-05-04 16:54:53 +1000

    feat: Remove unused contract

--- react-app/components/index.ts
+++ react-app/components/index.ts
@@ -1,4 +1,2 @@
-export * from "./StorageContract";
-export * from "./GreeterContract";
 export * from "./AccountInfo";
 export * from "./Polling";
--- react-app/pages/index.tsx
+++ react-app/pages/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import { Tabs, Tab, Typography, Box } from "@mui/material";
-// import deployedContracts from "@celo-progressive-dapp-starter/hardhat/deployments/hardhat_contracts.json";
 import { useContractKit } from "@celo-tools/use-contractkit";
-import { StorageContract, GreeterContract, AccountInfo, Polling } from "@/components";
+import { AccountInfo, Polling } from "@/components";
 import AppLayout from "@/components/layout/AppLayout";
 
 interface TabPanelProps {
@@ -19,30 +18,17 @@ export default function App() {
     setValue(newValue);
   };
 
-  // const contracts =
-  //   deployedContracts[network?.chainId?.toString()]?.[
-  //     network?.name?.toLocaleLowerCase()
-  //   ]?.contracts;
-
   return (
     <AppLayout title="Celo Starter" description="Celo Starter">
       <Box sx={{ width: "100%" }}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <Tabs variant="scrollable" scrollButtons allowScrollButtonsMobile value={value} onChange={handleChange} aria-label="basic tabs">
             <Tab label="Account" {...a11yProps(0)} />
-            <Tab label="Storage" {...a11yProps(1)} />
-            <Tab label="Greeter" {...a11yProps(2)} />
           </Tabs>
         </Box>
         <TabPanel value={value} index={0}>
           <AccountInfo></AccountInfo>
         </TabPanel>
-        {/*<TabPanel value={value} index={1}>*/}
-        {/*  <StorageContract contractData={contracts?.Storage} />*/}
-        {/*</TabPanel>*/}
-        {/*<TabPanel value={value} index={2}>*/}
-        {/*  <GreeterContract contractData={contracts?.Greeter} />*/}
-        {/*</TabPanel>*/}
       </Box>
       <Polling/>
     </AppLayout>
