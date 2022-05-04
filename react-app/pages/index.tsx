386371665dcedde6905d5ce6e73ae2cd07b914d3
Tuan Anh Le <anh.le@myob.com>
2022-05-04 22:15:06 +1000

    feat: fetch data improvement

--- react-app/pages/index.tsx
+++ react-app/pages/index.tsx
@@ -5,52 +5,57 @@ import AppLayout from "@/components/layout/AppLayout";
 
 import marketplaceAbi from '../contract/marketplace.abi.json';
 import erc20Abi from '../contract/erc20.abi.json';
+import {useEffect, useState} from "react";
 
 const ERC20_DECIMALS = 18;
 const MPContractAddress = "0xF377516621Cef90E12C0b5133adc783A336B1123";
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
 let contract;
-let products = [];
-
-// using contract to get product
-const getProducts = async function() {
-  const _productsLength = await contract.methods.getProductsLength().call()
-  const _products = []
-  for (let i = 0; i < _productsLength; i++) {
-    let _product = new Promise(async (resolve, reject) => {
-      let p = await contract.methods.readProduct(i).call()
-      resolve({
-        index: i,
-        owner: p[0],
-        name: p[1],
-        image: p[2],
-        description: p[3],
-        location: p[4],
-        price: new BigNumber(p[5]),
-        sold: p[6],
-      })
-    })
-    _products.push(_product)
-  }
-  // Wait until products are updated and render products list again
-  products = await Promise.all(_products)
-}
 
 export default function App() {
   // get contract kit
-  const { kit } = useContractKit();
+  const { kit, address, network } = useContractKit();
+  const [products, setProducts] = useState([]);
 
   // get contract of our marketplace
   // @ts-ignore
   contract = new kit.web3.eth.Contract(marketplaceAbi, MPContractAddress);
 
   // get products
-  getProducts().then(_ => console.log(`Number of products ${products.length}`));
+  useEffect(() => {
+    async function fetchProducts() {
+      const _productsLength = await contract.methods.getProductsLength().call()
+      const _products = []
+      for (let i = 0; i < _productsLength; i++) {
+        let _product = new Promise(async (resolve, reject) => {
+          let p = await contract.methods.readProduct(i).call()
+          resolve({
+            index: i,
+            owner: p[0],
+            name: p[1],
+            image: p[2],
+            description: p[3],
+            location: p[4],
+            price: new BigNumber(p[5]),
+            sold: p[6],
+          })
+        })
+        _products.push(_product)
+      }
+      // Wait until products are updated and render products list again
+      await Promise.all(_products)
+
+      setProducts(_products);
+    }
+
+    fetchProducts().then(_ => console.log(`fetch data successful !`));
+
+  }, [address, network]);
 
   return (
     <AppLayout title="Marketplace" description="A commune marketplace">
-      This is where i have to get products
+      Number of products in the marketplace {products.length}
     </AppLayout>
   );
 }
