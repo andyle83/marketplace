74c1a64e06a578578497821121f294531aec9997
Tuan Anh Le <anh.le@myob.com>
2022-05-05 07:59:30 +1000

    feat: code clean up

--- react-app/components/layout/Header.tsx
+++ react-app/components/layout/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
 
   useEffect(() => {
     if (address) {
-      fetchBalance().then(_ => console.log("fet balance successfully"))
+      fetchBalance().catch(e => console.error(e));
     }
   }, [network, address])
 
