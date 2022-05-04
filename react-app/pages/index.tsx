7a942c0d6896e84d703bcc06c9086d9ea08bed1c
Tuan Anh Le <anh.le@myob.com>
2022-05-05 07:52:37 +1000

    feat: code clean up

--- react-app/pages/index.tsx
+++ react-app/pages/index.tsx
@@ -1,18 +1,12 @@
 import * as React from "react";
+import {useEffect, useState} from "react";
+
 import BigNumber from "bignumber.js"
 import { useContractKit } from "@celo-tools/use-contractkit";
 import AppLayout from "@/components/layout/AppLayout";
-import Products from '../components/product/Products';
 
 import marketplaceAbi from '../contract/marketplace.abi.json';
-import erc20Abi from '../contract/erc20.abi.json';
-import {useEffect, useState} from "react";
-
-const ERC20_DECIMALS = 18;
 const MPContractAddress = "0xF377516621Cef90E12C0b5133adc783A336B1123";
-const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
-
-let contract;
 
 export default function App() {
   // get contract kit
@@ -21,7 +15,7 @@ export default function App() {
 
   // get contract of our marketplace
   // @ts-ignore
-  contract = new kit.web3.eth.Contract(marketplaceAbi, MPContractAddress);
+  const contract = new kit.web3.eth.Contract(marketplaceAbi, MPContractAddress);
 
   // get products
   useEffect(() => {
@@ -29,7 +23,7 @@ export default function App() {
       const _productsLength = await contract.methods.getProductsLength().call()
       const _products = []
       for (let i = 0; i < _productsLength; i++) {
-        let _product = new Promise(async (resolve, reject) => {
+        let _product = new Promise(async (resolve, _) => {
           let p = await contract.methods.readProduct(i).call()
           resolve({
             index: i,
@@ -55,9 +49,8 @@ export default function App() {
   }, [address, network]);
 
   const renderProducts = () => {
+    console.log(`number of products ${products.length}`);
     return products.map((product, index) => {
-      console.log(index)
-      console.log(product.name)
       return (
         <div className="col-md-4" key={index}>
           {product.name}
