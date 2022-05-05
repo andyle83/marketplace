5b68beb5ff968cffd9ef1163617dddfb95288755
Tuan Anh Le <anh.le@myob.com>
2022-05-05 23:18:39 +1000

    fix: simplify footer

--- react-app/components/layout/Footer.tsx
+++ react-app/components/layout/Footer.tsx
@@ -8,23 +8,28 @@ export default function Footer() {
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-shop" viewBox="0 0 16 16">
             <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
           </svg>
-          <small className="d-block mb-3 text-muted">© 2022-2050</small>
+          <small className="d-block mb-3 text-muted mt-2">© 2022-2050</small>
         </div>
-        <div className="col-6 col-md">
-          <h5>Features</h5>
-          <ul className="list-unstyled text-small">
-            <li><a className="text-muted" href="#">Cool stuff</a></li>
-            <li><a className="text-muted" href="#">Team feature</a></li>
-            <li><a className="text-muted" href="#">Stuff for developers</a></li>
-          </ul>
-        </div>
-        <div className="col-6 col-md">
-          <h5>About</h5>
-          <ul className="list-unstyled text-small">
-            <li><a className="text-muted" href="#">Team</a></li>
-            <li><a className="text-muted" href="#">Privacy</a></li>
-            <li><a className="text-muted" href="#">Terms</a></li>
-          </ul>
+        <div className="col-12 col-md">
+          <form action="">
+            <div className="row d-flex justify-content-center">
+              <div className="col-auto">
+                <p className="pt-2">
+                  <strong>Sign up newsletter</strong>
+                </p>
+              </div>
+              <div className="col-md-5 col-12">
+                <div className="form-outline form-white mb-4 bordered">
+                  <input type="email" id="form5Example21" className="form-control"/>
+                </div>
+              </div>
+              <div className="col-auto">
+                <button type="submit" className="btn btn-dark mb-4">
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </footer>
