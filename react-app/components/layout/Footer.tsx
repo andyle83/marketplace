f85140bc06e21beb301916386332983430f8d391
Tuan Anh Le <anh.le@myob.com>
2022-05-05 21:33:42 +1000

    fix: code format

--- react-app/components/layout/Footer.tsx
+++ react-app/components/layout/Footer.tsx
@@ -11,21 +11,21 @@ export default function Footer() {
           <small className="d-block mb-3 text-muted">© 2022-2050</small>
         </div>
         <div className="col-6 col-md">
-        <h5>Features</h5>
-        <ul className="list-unstyled text-small">
-          <li><a className="text-muted" href="#">Cool stuff</a></li>
-          <li><a className="text-muted" href="#">Team feature</a></li>
-          <li><a className="text-muted" href="#">Stuff for developers</a></li>
-        </ul>
-      </div>
+          <h5>Features</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="text-muted" href="#">Cool stuff</a></li>
+            <li><a className="text-muted" href="#">Team feature</a></li>
+            <li><a className="text-muted" href="#">Stuff for developers</a></li>
+          </ul>
+        </div>
         <div className="col-6 col-md">
-        <h5>About</h5>
-        <ul className="list-unstyled text-small">
-          <li><a className="text-muted" href="#">Team</a></li>
-          <li><a className="text-muted" href="#">Privacy</a></li>
-          <li><a className="text-muted" href="#">Terms</a></li>
-        </ul>
-      </div>
+          <h5>About</h5>
+          <ul className="list-unstyled text-small">
+            <li><a className="text-muted" href="#">Team</a></li>
+            <li><a className="text-muted" href="#">Privacy</a></li>
+            <li><a className="text-muted" href="#">Terms</a></li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
