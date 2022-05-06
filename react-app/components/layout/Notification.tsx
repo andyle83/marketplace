b75cf851f5741d9227269a512fe48b047608282a
Tuan Anh Le <anh.le@myob.com>
2022-05-06 23:39:46 +1000

    feat: put notification message in our store

--- react-app/components/layout/Notification.tsx
+++ react-app/components/layout/Notification.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import {useSelector, RootStateOrAny } from "react-redux";
 
 export default function Notification() {
-  const isLoading = useSelector((state:RootStateOrAny) => state.app.isLoading );
-  const notificationMessage = useSelector((state:RootStateOrAny) => state.app.notificationMessage);
+  const [isLoading, notificationMessage] = useSelector(
+    (state:RootStateOrAny) => [state.app.isLoading, state.app.notificationMessage]
+  );
 
   return isLoading && (notificationMessage != "") && (
     <div className="alert alert-warning sticky-top mt-2" role="alert">
