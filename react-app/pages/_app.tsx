ac67003868bec22235357fc4a570d8c516c273e8
Tuan Anh Le <anh.le@myob.com>
2022-05-06 20:32:39 +1000

    feat: upgrade package

--- react-app/pages/_app.tsx
+++ react-app/pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "@celo-tools/use-contractkit/lib/styles.css";
-import { SnackbarProvider } from "notistack";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/apollo-client";
 import { Alfajores, ContractKitProvider } from "@celo-tools/use-contractkit";
@@ -19,33 +18,26 @@ function Updaters() {
 }
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
+  const AnyComponent = Component as any;
   return (
     <Provider store={store}>
         <ContractKitProvider
           dapp={{
-            name: "use-contractkit demo",
-            description: "A commune marketplace",
+            name: "market place",
+            description: "a commune marketplace based on Celo blockchain",
             url: "https://use-contractkit.vercel.app",
             icon: "https://use-contractkit.vercel.app/favicon.ico",
           }}
           network={Alfajores}
         >
-          <SnackbarProvider
-            maxSnack={3}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "right",
-            }}
-          >
             <Updaters/>
             <ApolloProvider client={client}>
               <div suppressHydrationWarning>
                 {typeof window === "undefined" ? null : (
-                  <Component {...pageProps} />
+                  <AnyComponent {...pageProps} />
                 )}
               </div>
             </ApolloProvider>
-          </SnackbarProvider>
         </ContractKitProvider>
     </Provider>
   );
