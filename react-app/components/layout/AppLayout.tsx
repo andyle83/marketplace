3d7aa0d109fdcd206251535115d2e72754d6e847
Tuan Anh Le <anh.le@myob.com>
2022-05-10 21:32:57 +1000

    feat: code clean up

--- react-app/components/layout/AppLayout.tsx
+++ react-app/components/layout/AppLayout.tsx
@@ -3,12 +3,10 @@ import Footer from "./Footer";
 import { Header } from "./Header";
 
 interface AppLayoutProps {
-  title: string;
-  description: string;
   children: React.ReactNode;
 }
 
-export default function AppLayout({ title, description, children }: AppLayoutProps) {
+export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="container mt-2" style={{ maxWidth: "72em"}}>
       <Header />
