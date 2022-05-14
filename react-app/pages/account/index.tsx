e8f3da440b62a3c8ff01c7036d3527e7bd24f5b3
Tuan Anh Le <anh.le@myob.com>
2022-05-14 10:37:42 +1000

    feat: suggest another ui of order history

--- react-app/pages/account/index.tsx
+++ react-app/pages/account/index.tsx
@@ -5,42 +5,37 @@ export default function Account() {
   return (
     <AppLayout>
       <main id="marketplace">
-        <div className="table-responsive p-3">
-          <table className="table">
-            <caption>Your order History</caption>
-            <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Time</th>
-              <th scope="col">Order</th>
-              <th scope="col">Total</th>
-              <th scope="col">Transaction</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>14/05/2022</td>
-              <td>Giant BBQ</td>
-              <td>120 cUSD</td>
-              <td>@812381883312013801</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>16/05/2022</td>
-              <td>BBQ Chicken</td>
-              <td>40 cUSD</td>
-              <td>@812381883312013801</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>17/05/2022</td>
-              <td>Beef burrito</td>
-              <td>90 cUSD</td>
-              <td>@812381883312013801</td>
-            </tr>
-            </tbody>
-          </table>
+        <h5 className="pt-3 pb-3">Your order history</h5>
+        <div className="card">
+          <div className="card-header">
+            <div className="row">
+              <div className="col-sm">
+                Order Place
+              </div>
+              <div className="col-sm">
+                Total
+              </div>
+              <div className="col-sm">
+                Transaction
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm">
+                20/05/2022
+              </div>
+              <div className="col-sm">
+                230 cUSD
+              </div>
+              <div className="col-sm">
+                0x8787HHKHGHBJ
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">Giant BBQ</h5>
+            <p className="card-text">Any details information of this order.</p>
+            <a href="#" className="btn btn-outline-primary">Buy Again</a>
+          </div>
         </div>
       </main>
     </AppLayout>
