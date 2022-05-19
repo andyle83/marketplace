8197569608986554b3e924d96c1584cdc1079ff5
Tuan Anh Le <anh.le@myob.com>
2022-05-19 21:27:25 +1000

    feat: update profile (address and balance) when wallet connected or disconnected

--- react-app/components/user/Wallet.tsx
+++ react-app/components/user/Wallet.tsx
@@ -1,20 +1,31 @@
 import * as React from "react";
 import {useContractKit} from "@celo-tools/use-contractkit";
 import {useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
+import {updateProfile} from "@/state/app/reducer";
 
 export default function Wallet() {
   const { address, network, kit, connect, destroy } = useContractKit();
   const [balance, setBalance] = useState<string>('');
+  const dispatch = useDispatch();
 
-  async function fetchBalance() {
+  const  fetchBalance = async () => {
     const { cUSD } = await kit.getTotalBalance(address);
     const roundingBalance = parseFloat(kit.web3.utils.fromWei(cUSD.toString(), 'ether')).toFixed(2);
     setBalance(roundingBalance);
   }
 
+  const disconnect = async () => {
+    await destroy();
+    dispatch(updateProfile( { address: null, balance: '' }));
+  }
+
   useEffect(() => {
     if (address) {
       fetchBalance().catch(e => console.error(e));
+
+      // update profile (address & balance)
+      dispatch(updateProfile({ address, balance }));
     }
   }, [network, address])
 
@@ -27,7 +38,7 @@ export default function Wallet() {
     ) :
     (
       <button type="button" className="btn btn-outline-primary" style={{display: "flex", alignItems: "center"}}>
-        <i className="bi bi-x-circle" onClick={destroy} style={{marginRight: "0.5rem"}}></i>
+        <i className="bi bi-x-circle" onClick={disconnect} style={{marginRight: "0.5rem"}}></i>
         <span id="balance" >{balance}</span>cUSD
       </button>
     )
