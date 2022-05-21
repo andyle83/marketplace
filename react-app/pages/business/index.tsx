2ba2cebc668a4bcc9a9bae95f6b381e722224543
Tuan Anh Le <anh.le@myob.com>
2022-05-21 19:45:13 +1000

    feat: form css

--- react-app/pages/business/index.tsx
+++ react-app/pages/business/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import AppLayout from "@/components/layout/AppLayout";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { object, string, number, mixed } from "yup";
+import { object, string } from "yup";
 import { useContractKit } from "@celo-tools/use-contractkit";
 import { BusinessRegisterLabel, BusinessWalletRequest } from "@/constants";
 import {yupResolver} from "@hookform/resolvers/yup";
@@ -46,35 +46,35 @@ const Business: React.FC = (): JSX.Element => {
             </div>
             <div className="card-body">
               <form onSubmit={handleSubmit(onSubmit)} id="newBusiness">
-                <div className="row mb-4">
+                <div className="row mb-3 mt-3 justify-content-center">
                   <label htmlFor="name" className="col-sm-2 col-form-label">Business Name</label>
                   <div className="col-sm-6">
                     <input type="text" className="form-control" id="name" {...register("name", { required: true })}  />
                   </div>
                   <div role="alert" className="mt-2 text-danger">{errors.name?.message}</div>
                 </div>
-                <div className="row mb-4">
+                <div className="row mb-4 justify-content-center">
                   <label htmlFor="address" className="col-sm-2 col-form-label">Wallet Address</label>
                   <div className="col-sm-6">
                     <input type="text" className="form-control" disabled id="name" value={address} />
                   </div>
                 </div>
-                <div className="row mb-4">
-                  <label htmlFor="location" className="col-sm-2 col-form-label">Location</label>
+                <div className="row mb-4 justify-content-center">
+                  <label htmlFor="location" className="col-sm-2 col-form-label">Register Location</label>
                   <div className="col-sm-6">
                     <input type="text" className="form-control" id="location" {...register("location", { required: true })}  />
                   </div>
                   <div role="alert" className="mt-2 text-danger">{errors.location?.message}</div>
                 </div>
-                <div className="row mb-4">
-                  <label htmlFor="phone" className="col-sm-2 col-form-label">Phone</label>
+                <div className="row mb-4 justify-content-center">
+                  <label htmlFor="phone" className="col-sm-2 col-form-label">Mobile Number</label>
                   <div className="col-sm-6">
                     <input type="text" className="form-control" id="phone" {...register("phone", { required: true })}  />
                   </div>
                   <div role="alert" className="mt-2 text-danger">{errors.phone?.message}</div>
                 </div>
-                <div className="row mb-4">
-                  <div className="col-sm-2">
+                <div className="row mb-4 justify-content-center">
+                  <div className="col-sm-4">
                     <button type="submit" className="btn btn-outline-primary tw-mr-2" form="newBusiness">Submit</button>
                     <button type="reset" className="btn btn-outline-primary" form="newBusiness">Reset</button>
                   </div>
