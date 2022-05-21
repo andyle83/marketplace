349dda57b9b19a0c288185032276a9d6fa6db6b1
Tuan Anh Le <anh.le@myob.com>
2022-05-21 18:53:09 +1000

    feat: adding business form

--- react-app/pages/business/index.tsx
+++ react-app/pages/business/index.tsx
@@ -1,12 +1,39 @@
 import * as React from "react";
 import AppLayout from "@/components/layout/AppLayout";
+import { object, string, number, mixed } from "yup";
+import { useContractKit } from "@celo-tools/use-contractkit";
+import { BusinessWalletRequest } from "@/constants";
 
-const Business: React.FC = () => {
+type IFormInputs = {
+  name: string;
+  address: string;
+  location: string;
+  phone: string;
+}
+
+const validBusinessSchema = object({
+  name: string().required("Required Full Name"),
+  address: string().required("Wallet Address"),
+  location: string().required("Register Business Location"),
+  phone: string().required("Contact Phone Number")
+}).required();
+
+const Business: React.FC = (): JSX.Element => {
+  const { kit, address } = useContractKit();
   return (
     <AppLayout>
-      <main id="marketplace">
-        Using this page as an apply form for Business (Customer to Business)
-      </main>
+      {!address ? <div className="text-center p-5">{BusinessWalletRequest}</div> :
+        <main id="marketplace">
+          <div className="card mb-4">
+            <div className="card-header">
+              Business Apply Form
+            </div>
+            <div className="card-body">
+              Here is form fields
+            </div>
+          </div>
+        </main>
+      }
     </AppLayout>
   )
 }
