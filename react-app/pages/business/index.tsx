77f3404cad2bdfc1fbf5190ca5736bfe080c1ac2
Tuan Anh Le <anh.le@myob.com>
2022-05-21 18:38:20 +1000

    fix: business page

---
+++ react-app/pages/business/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import AppLayout from "@/components/layout/AppLayout";
+
+const Business: React.FC = () => {
+  return (
+    <AppLayout>
+      <main id="marketplace">
+        Using this page as an apply form for Business (Customer to Business)
+      </main>
+    </AppLayout>
+  )
+}
+
+export default Business;
